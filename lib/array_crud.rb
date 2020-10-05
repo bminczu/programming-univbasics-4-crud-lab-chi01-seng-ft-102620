cc102edf07cd88f5d91b7265e9ca3ec97a1e5d89
adventurous-format-6947 <bminczu@gmail.com>
2020-10-05 16:44:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-array.pop("arrays!")
+remove_element_from_end_of_array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
